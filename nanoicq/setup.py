a917a942f7ea316ae23e60e0a3404493a4902331
lightdruid <lightdruid>
2006-01-23 21:58:39 +0000

    improving list control and icons

---
+++ nanoicq/setup.py
@@ -0,0 +1,14 @@
+
+#
+# $Id: setup.py,v 1.1 2006/01/23 21:58:39 lightdruid Exp $
+#
+
+# Just a sample script for nanoicq, it does nothing
+# useful for this moment
+
+from distutils.core import setup
+import py2exe
+
+setup(console = ["wxnanoicq.py"])
+
+# ---
