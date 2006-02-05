84fd96c35edae0cb3dde8b2a656eca6adbe619b4
lightdruid <lightdruid>
2006-02-05 16:07:19 +0000

    trying to force setup.py to generate valid rpm spec

--- nanoicq/setup.py
+++ nanoicq/setup.py
@@ -1,14 +1,36 @@
 
 #
-# $Id: setup.py,v 1.1 2006/01/23 21:58:39 lightdruid Exp $
+# $Id: setup.py,v 1.2 2006/02/05 16:07:19 lightdruid Exp $
 #
 
 # Just a sample script for nanoicq, it does nothing
 # useful for this moment
 
-from distutils.core import setup
-import py2exe
+import sys, glob
 
-setup(console = ["wxnanoicq.py"])
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+
+datafiles=[
+        ('icons/aox', glob.glob('icons/aox/*.ico'))
+    ] # only works for bdist and friends
+
+if sys.platform.find("win32", 0, 5) == 0:
+    platform="WIN32"
+else:
+    platform="UN*X"
+
+setup(name = 'NanoICQ',
+      version = '0.1',
+      description = 'ICQ client',
+      author = 'Andrey Sidorenko',
+      author_email = 'sidorenko@gmail.com',
+      url = "http://nanoicq.berlios.de",
+      license = "MIT",
+      long_description = """ """,
+      platforms = [platform],
+      packages=['sandbox','sandbox.ui'],
+      data_files = datafiles)
 
 # ---
