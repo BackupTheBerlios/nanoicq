bad310ee92efe4fa1fffcf32ce85c348d6bfb213
lightdruid <lightdruid>
2006-03-17 09:47:54 +0000

    no message

--- nanoicq/sandbox/ui/UserInfo.py
+++ nanoicq/sandbox/ui/UserInfo.py
@@ -1,6 +1,6 @@
 
 #
-# $Id: UserInfo.py,v 1.3 2006/03/16 16:01:07 lightdruid Exp $
+# $Id: UserInfo.py,v 1.4 2006/03/17 09:47:54 lightdruid Exp $
 #
 
 import sys
@@ -27,15 +27,65 @@ class _Pane_auto:
 
     def _pre(self, items):
         for n in items:
-            nn = eval("wx.TextCtrl(self, -1, '', style = wx.NO_BORDER, name = '%s')" % n)
+            nn = eval("wx.TextCtrl(self, -1, '', style = wx.NO_BORDER | wx.TE_READONLY, name = '%s')" % n)
 
-    def _put_item(self, name):
+    def _put_item(self, name, val = None):
         self.sz.Add(self.FindWindowByName(name), row = self.r, col = self.c + 2)
         self.FindWindowByName(name).SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_MENU))
         if hasattr(self.b, name) and eval('self.b.%s' % name) is not None:
             self.FindWindowByName(name).SetValue(eval('self.b.%s' % name))
+        elif val is not None:
+            self.FindWindowByName(name).SetValue(val)
 
-class Pane_AvatarZZZ(wx.Panel, _Pane_auto):
+class Pane_Notes(wx.Panel, _Pane_auto):
+    def __init__(self, parent, b):
+        wx.Panel.__init__(self, parent, -1)
+        _Pane_auto.__init__(self)
+
+        self.sz = wx.BoxSizer(wx.VERTICAL)
+        sz = self.sz
+
+        self.notes = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE | wx.TE_READONLY)
+        self.notes.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_MENU))
+
+        self.my_notes = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE)
+
+        sz.Add(wx.StaticText(self, -1, 'About:'), 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, 7)
+        sz.Add(self.notes, 1, wx.ALL | wx.EXPAND, 7)
+        sz.Add(wx.StaticText(self, -1, 'My notes:'), 0, wx.LEFT | wx.RIGHT | wx.EXPAND, 7)
+        sz.Add(self.my_notes, 1, wx.ALL | wx.EXPAND, 7)
+
+        # ---
+        self.SetSizer(sz)
+        self.SetAutoLayout(True)
+
+class Pane_Background(wx.Panel, _Pane_auto):
+    def __init__(self, parent, b):
+        wx.Panel.__init__(self, parent, -1)
+        _Pane_auto.__init__(self)
+
+        self.sz = wx.BoxSizer(wx.VERTICAL)
+        sz = self.sz
+
+        self.web = wx.TextCtrl(self, -1, '', style = wx.NO_BORDER | wx.TE_READONLY)
+        self.web.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_MENU))
+        self.web.SetValue('<none>')
+
+        self.past_background = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE | wx.TE_READONLY)
+        self.interests = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE | wx.TE_READONLY)
+
+        sz.Add(wx.StaticText(self, -1, 'Web site:'), 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, 7)
+        sz.Add(self.web, 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, 7)
+        sz.Add(wx.StaticText(self, -1, 'Past background:'), 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, 7)
+        sz.Add(self.past_background, 10, wx.ALL | wx.EXPAND, 7)
+        sz.Add(wx.StaticText(self, -1, 'Interests:'), 0, wx.LEFT | wx.RIGHT | wx.EXPAND, 7)
+        sz.Add(self.interests, 10, wx.ALL | wx.EXPAND, 7)
+
+        # ---
+        self.SetSizer(sz)
+        self.SetAutoLayout(True)
+
+class Pane_Work(wx.Panel, _Pane_auto):
     def __init__(self, parent, b):
         wx.Panel.__init__(self, parent, -1)
         _Pane_auto.__init__(self)
@@ -44,15 +94,106 @@ class Pane_AvatarZZZ(wx.Panel, _Pane_auto):
         self.sz = rcs.RowColSizer()
         sz = self.sz
 
-        self._pre(['', '', 'internal_ip', 'port', 
-            'protocol_version', 'user_client', 'online_since', 
-            'system_up_since', 'idle_since'])
+        self._pre(['company', 'department', 'position', 'work_street', 
+            'work_city', 'work_state', 'work_zip', 'work_country', 'work_web'])
         g = self._put_item
 
         self.c = 1
         self.r = 1
+        sz.Add(wx.StaticText(self, -1, 'Company:'), row = self.r, col = self.c)
+        g('company')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Department:'), row = self.r, col = self.c)
+        g('department')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Position:'), row = self.r, col = self.c)
+        g('position')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Street:'), row = self.r, col = self.c)
+        g('work_street')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'City:'), row = self.r, col = self.c)
+        g('work_city')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'State:'), row = self.r, col = self.c)
+        g('work_state')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Postal code:'), row = self.r, col = self.c)
+        g('work_zip')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Country:'), row = self.r, col = self.c)
+        g('work_country')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Web site:'), row = self.r, col = self.c)
+        g('work_web')
+        self.r += 1
+
+        # ---
+        self.SetSizer(sz)
+        self.SetAutoLayout(True)
+
+class Pane_Location(wx.Panel, _Pane_auto):
+    def __init__(self, parent, b):
+        wx.Panel.__init__(self, parent, -1)
+        _Pane_auto.__init__(self)
+
+        self.b = b
+        self.sz = rcs.RowColSizer()
+        sz = self.sz
+
+        self._pre(['street', 'city', 'state', 'zip', 'country', 'lang1', 'lang2', 'lang3', 'timezone', 'local_time'])
+        g = self._put_item
+
+        self.c = 1
+        self.r = 1
+        sz.Add(wx.StaticText(self, -1, 'Street:'), row = self.r, col = self.c)
+        g('street')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'City:'), row = self.r, col = self.c)
+        g('city')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'State:'), row = self.r, col = self.c)
+        g('state')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Postal code:'), row = self.r, col = self.c)
+        g('zip')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Country:'), row = self.r, col = self.c)
+        g('country')
+        self.r += 1
+
+        self.c = 4
+        self.r = 1
+        sz.Add(wx.StaticText(self, -1, 'Languages:'), row = self.r, col = self.c)
+        g('lang1')
+        self.r += 1
+
         sz.Add(wx.StaticText(self, -1, ''), row = self.r, col = self.c)
-        g('')
+        g('lang2')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, ''), row = self.r, col = self.c)
+        g('lang3')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Time zone:'), row = self.r, col = self.c)
+        g('timezone')
+        self.r += 1
+
+        sz.Add(wx.StaticText(self, -1, 'Local time:'), row = self.r, col = self.c)
+        g('local_time')
         self.r += 1
 
         # ---
@@ -67,8 +208,8 @@ class Pane_Contact(wx.Panel, _Pane_auto):
         self.sz = wx.BoxSizer(wx.VERTICAL)
         sz = self.sz
 
-        self.mails = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE)
-        self.phones = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE)
+        self.mails = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE | wx.TE_READONLY)
+        self.phones = wx.TextCtrl(self, -1, '', style = wx.TE_MULTILINE | wx.TE_READONLY)
 
         sz.Add(wx.StaticText(self, -1, 'E-mail:'), 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, 7)
         sz.Add(self.mails, 1, wx.ALL | wx.EXPAND, 7)
@@ -241,7 +382,7 @@ class UserInfoPanel(wx.Panel):
                 self.nb.AddPage(win, h)
             except NameError, msg:
                 print msg
-                #raise
+                raise
 
         self.wp = WhitePane(self, self.iconSet['main'], b)
         sz.Add(self.wp, 1, wx.EXPAND | wx.ALL, 0)
@@ -272,7 +413,7 @@ class UserInfoPanel(wx.Panel):
 
 class UserInfoFrame(wx.Frame):
     def __init__(self, parentFrame, ID, iconSet, b, title = 'User info',
-            size = (370, 370), pos = wx.DefaultPosition,
+            size = (370, 380), pos = wx.DefaultPosition,
             style = wx.DEFAULT_DIALOG_STYLE | wx.MAXIMIZE_BOX | wx.MINIMIZE_BOX):
 
         wx.Frame.__init__(self, None, ID, size = size, style = style,
