e739762e3ad9613940159f53891b53ded9887b4a
lightdruid <lightdruid>
2006-04-04 20:57:33 +0000

    no message

--- nanoicq/config-devel/pt.cpp
+++ nanoicq/config-devel/pt.cpp
@@ -10,6 +10,7 @@
 
 #define ZZ std::cerr << "line: " << __LINE__ << std::endl;
 
+using namespace std;
 
 template <class K, class D>
 class PatriciaTree;
@@ -35,6 +36,9 @@ public:
     PatriciaTreeNode(const K& nkey, const D& ndata,
         const TPT& nright, const TPT& nleft, const int nsplit_bit);
     PatriciaTreeNode& operator=(const PatriciaTreeNode& ptn);
+
+    void init(const K& nkey, const D& ndata,
+        const TPT& nright, const TPT& nleft, const int nsplit_bit);
 
     void setData(const D& d) {
         D tmp(d);
@@ -69,8 +73,22 @@ PatriciaTreeNode<K, D>::PatriciaTreeNode() : key(), data(), split_bit(-1) {
 template <class K, class D>
 PatriciaTreeNode<K, D>::PatriciaTreeNode(const K& nkey, const D& ndata,
     const TPT& nright, const TPT& nleft, const int nsplit_bit)
-        : key(nkey), data(ndata), left(nleft), right(nright), 
+        : left(nleft), right(nright), 
             split_bit(nsplit_bit) {
+
+    key = strdup(nkey);
+    data = ndata;
+}
+
+template <class K, class D>
+void PatriciaTreeNode<K, D>::init(const K& nkey, const D& ndata,
+    const TPT& nright, const TPT& nleft, const int nsplit_bit) {
+
+    left = nleft;
+    right = nright;
+    split_bit = nsplit_bit;
+    key = strdup(nkey);
+    data = ndata;
 }
 
 template <class K, class D>
@@ -117,8 +135,8 @@ PatriciaTreeNode<K, D>::operator!=(const PatriciaTreeNode& ptn) const {
 template <class K, class D>
 class PatriciaTree {
 
-    typedef PatriciaTreeNode<K, D> node;
-    typedef node* pnode;
+    typedef typename PatriciaTreeNode<K, D> node;
+    typedef typename node* pnode;
 
     pnode root;
 
@@ -129,15 +147,26 @@ class PatriciaTree {
         return k1 == k2;
     }
 
+    template <>
+    bool compare(char* k1, char* k2) {
+            //std::cerr << "compare" << std::endl;
+        if(!k1 || !k2) {
+            return false;
+        }
+            //std::cerr << k1 << ":" << k2 << std::endl;
+        return strcmp(k1, k2) == 0;
+    }
+
     // Will fail if bit number > std::numeric_limits<int>::max()
     int findSplitBit(const K& k1, const K& k2);
 
 public:
     PatriciaTree() {
         root = new node();
+        root->key = K();
     }
 
-    bool insert(K key, D data);
+    pnode insert(K key, D data);
 };
 
 template <class K, class D>
@@ -150,11 +179,11 @@ int PatriciaTree<K, D>::getBit(K d, int n) {
     const int bit = n & 7;
 
     return (d[byte] & (1 << bit));
-
 }
 
 template <class K, class D>
-bool PatriciaTree<K, D>::insert(K key, D data) {
+typename PatriciaTree<K, D>::pnode
+PatriciaTree<K, D>::insert(K key, D data) {
     pnode p = root;
     pnode r = p->right;
 
@@ -164,7 +193,7 @@ bool PatriciaTree<K, D>::insert(K key, D data) {
     }
 
     if(compare(key, r->key))
-        return false;
+        return 0;
 
     // split bit
     int sb = findSplitBit(key, r->key);
@@ -175,7 +204,7 @@ bool PatriciaTree<K, D>::insert(K key, D data) {
     pnode n = p->right;
 
     // can we remember n->split_bit < sb pos in upper cycle?
-    while(p->split_bit < n->split_bit && n->split_bit < sb) {
+    while((p->split_bit < n->split_bit) && (n->split_bit < sb)) {
         p = n;
         n = getBit(key, n->split_bit) ? n->right : n->left;
     }
@@ -183,12 +212,14 @@ bool PatriciaTree<K, D>::insert(K key, D data) {
     // create a new internal node
     int nb = getBit(key, sb);
     assert(nb >= 0);
-    pnode newNode = new node(key, data, nb ? n : r, nb ? r : n, sb);
+
+    pnode newNode = new node();
+    newNode->init(key, data, nb ? newNode : n, nb ? n : newNode, sb);
 
     // balance
     getBit(key, p->split_bit) ? p->right = newNode : p->left = newNode;
 
-    return true;
+    return newNode;
 }
 
 
@@ -196,20 +227,47 @@ template <class K, class D>
 int PatriciaTree<K, D>::findSplitBit(const K& k1, const K& k2) {
     int byte = 0;
 
-    if(!k1 || !k2)
+    if(!k1 || !k2) {
         return 0;
+    }
 
     for(; k1[byte] != 0 && k2[byte] != 0 && k1[byte] == k2[byte]; ++byte) {
         // empty
     }
 
     int bit = 0;
-    for(; getBit(&k1[byte], bit) != getBit(&k2[byte], bit); ++bit) {
+    for(; getBit(&k1[byte], bit) == getBit(&k2[byte], bit); ++bit) {
         // empty
     }
 
     return (8 * byte) + bit;
 }
+
+class FX {
+    char* c_;
+public:
+    FX() : c_(0) {}
+    FX(const char* c) {
+        c_ = strdup(c);
+    }
+
+    FX(char c) {
+        c_ = new char;
+        c_[0] = c;
+    }
+
+    ~FX() {
+        if(c_)
+            delete c_;
+    }
+
+    const int operator[](int ndx) const {
+        return (int)c_[ndx];
+    }
+    int operator[](int ndx) {
+        return (int)c_[ndx];
+    }
+};
 
 int main() {
     PatriciaTreeNode<char*, int> ptn1;
@@ -229,9 +287,33 @@ int main() {
     assert(strcmp("abcd", ptn5.getData()) == 0);
 
     PatriciaTree<char*, int>* ptn6 = new PatriciaTree<char*, int>();
-    ptn6->insert("string 100", 100);
+
+    assert(ptn6->insert("string 100", 100) != 0);
+    assert(ptn6->insert("string 100", 100) == 0);
+    assert(ptn6->insert("string 10", 10) != 0);
 
     delete ptn6;
+
+    // ---
+    PatriciaTree<char*, int>* ptn7 = new PatriciaTree<char*, int>();
+    const int MX = 100;
+    for(int ii = 0; ii < MX; ++ii) {
+        static char c[10];
+        sprintf(c, "string #%d", ii);
+        assert(ptn7->insert(c, ii) != 0);
+    }
+
+    assert(ptn7->insert("string #55", 55) == 0);
+    assert(ptn7->insert("string #100", ++ii) != 0);
+
+    delete ptn7;
+
+    // ---
+    PatriciaTree<FX, int>* ptn8 = new PatriciaTree<FX, int>();
+    //FX* fx = new FX('1');
+    //ptn8->insert(FX('1'), 1);
+    delete ptn8;
+
     std::cout << "Ok" << std::endl;
 
     return 0;
