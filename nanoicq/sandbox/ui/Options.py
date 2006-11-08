193f5578455a7de25038e7e622da524d71171d1c
lightdruid <lightdruid>
2006-11-08 12:55:04 +0000

    no message

--- nanoicq/sandbox/ui/Options.py
+++ nanoicq/sandbox/ui/Options.py
@@ -1,6 +1,6 @@
 
 #
-# $Id: Options.py,v 1.2 2006/11/08 11:48:32 lightdruid Exp $
+# $Id: Options.py,v 1.3 2006/11/08 12:55:04 lightdruid Exp $
 #
 
 import elementtree.ElementTree as ET
@@ -33,10 +33,21 @@ class OptionsNB(wx.Notebook):
                              #wx.NB_RIGHT
                              )
 
+class _Pane_Core:
+    def __init__(self):
+        pass
 
-class Pane_General(wx.Panel):
+    def store(self):
+        return ET.Element("_Pane_Core")
+
+    def restore(self, xml):
+        return
+
+
+class Pane_General(wx.Panel, _Pane_Core):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
+        _Pane_Core.__init__(self)
 
         self.sz = wx.BoxSizer(wx.VERTICAL)
         sz = self.sz
@@ -45,9 +56,10 @@ class Pane_General(wx.Panel):
         self.SetSizer(sz)
         self.SetAutoLayout(True)
 
-class Pane_UI(wx.Panel):
+class Pane_UI(wx.Panel, _Pane_Core):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
+        _Pane_Core.__init__(self)
 
         self.sz = wx.BoxSizer(wx.VERTICAL)
         sz = self.sz
@@ -57,9 +69,10 @@ class Pane_UI(wx.Panel):
         self.SetAutoLayout(True)
 
 
-class Pane_Network(wx.Panel):
+class Pane_Network(wx.Panel, _Pane_Core):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
+        _Pane_Core.__init__(self)
 
         self.sz = wx.BoxSizer(wx.VERTICAL)
         sz = self.sz
@@ -68,18 +81,87 @@ class Pane_Network(wx.Panel):
         self.SetSizer(sz)
         self.SetAutoLayout(True)
 
-class Pane_ICQ(wx.Panel):
+class Pane_ICQ(wx.Panel, _Pane_Core):
+    _DEFAULT_LOGIN_SERVER = wx.NewId()
+
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
+        _Pane_Core.__init__(self)
+
+        self._panelName = "ICQ"
 
         self.sz = wx.BoxSizer(wx.VERTICAL)
         sz = self.sz
 
-        sz.Add(wx.TextCtrl(self, -1, "asddddddddddddd"))
+        ssz1 = wx.StaticBoxSizer(wx.StaticBox(self, -1, 'ICQ'), wx.VERTICAL)
+
+        self.icqNumber = wx.TextCtrl(self, -1, "")
+        self.icqPassword = wx.TextCtrl(self, -1, "", style=wx.TE_PASSWORD)
+
+        self.hl = hyperlink.HyperLinkCtrl(self, -1, "Create new account using ICQ website", URL = "http://www.icq.com/register/")
+
+        rc = rcs.RowColSizer()
+
+        rc.Add(wx.StaticText(self, -1, "ICQ Number:" ), flag=wx.RIGHT, row=0, col=0)
+        rc.Add(wx.StaticText(self, -1, "Password:" ), flag=wx.ALIGN_RIGHT, row=1, col=0)
+
+        rc.Add(self.icqNumber, row=0, col=2)
+        rc.Add(self.icqPassword, row=1, col=2)
+
+        ssz1.Add(rc, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+        ssz1.Add(self.hl, 0, wx.ALL, 5)
+
+        # ---
+        ssz2 = wx.StaticBoxSizer(wx.StaticBox(self, -1, 'Connection settings'), wx.VERTICAL)
+
+        hs = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.loginServer = wx.TextCtrl(self, -1, "")
+        self.port = wx.TextCtrl(self, -1, "")
+        self.setDefaultLoginServer = wx.Button(self, self._DEFAULT_LOGIN_SERVER, "Default")
+
+        hs.Add(wx.StaticText(self, -1, "Login Server:"), 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        hs.Add(self.loginServer, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        hs.Add(wx.StaticText(self, -1, "Port:"), 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        hs.Add(self.port, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        hs.Add(self.setDefaultLoginServer, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        self.Bind(wx.EVT_BUTTON, self.onSetDefaultLoginServer, id=self._DEFAULT_LOGIN_SERVER)
+
+        ssz2.Add(hs, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
+        sz.Add(ssz1)
+        sz.Add(ssz2)
+
+        self.x = {}
+        self.x["ICQNumber"] = self.icqNumber.GetId()
+        self.x["ICQPassword"] = self.icqPassword.GetId()
+        self.x["LoginServer"] = self.loginServer.GetId()
+        self.x["Port"] = self.port.GetId()
 
         # ---
         self.SetSizer(sz)
         self.SetAutoLayout(True)
+
+    def onSetDefaultLoginServer(self, evt):
+        self.loginServer.SetValue("login.icq.com")
+        self.port.SetValue("5190")
+
+        xml = self.store()
+        self.restore(xml)
+
+    def restore(self, xml):
+        for c in xml.getchildren():
+            self.FindWindowById(self.x[c.tag]).SetValue(c.text)
+
+    def store(self):
+        root = ET.Element(self._panelName)
+        for k in self.x:
+            e = ET.Element(k)
+            e.text = self.FindWindowById(self.x[k]).GetValue()
+            root.append(e)
+
+        return root
 
 class OptionsTree(wx.TreeCtrl):
     def __init__(self, parent, id):
@@ -185,7 +267,7 @@ class OptionsPanel(wx.Panel):
         sz.Add(self.tz, 7, wx.EXPAND | wx.ALL, 5)
         sz.Add(hz, 0, wx.ALL | wx.ALIGN_RIGHT | wx.ALIGN_BOTTOM, 5)
 
-        self.okButton.Enable(False)
+        #self.okButton.Enable(False)
 
         self.Bind(wx.EVT_BUTTON, self.onOkButton, id = _ID_OK_BUTTON)
         self.Bind(wx.EVT_BUTTON, self.onCancelButton, id = _ID_CANCEL_BUTTON)
@@ -206,7 +288,8 @@ class OptionsPanel(wx.Panel):
         self._currentData = None
 
     def onOkButton(self, evt):
-        pass
+        for p in self.panes:
+            print ET.tostring(self.panes[p].store())
 
     def onCancelButton(self, evt):
         self._parent.Close()
@@ -306,7 +389,7 @@ class OptionsPanel1(wx.Panel):
 
 class OptionsFrame(wx.Frame):
     def __init__(self, parentFrame, ID, title = 'Options',
-            size = (570, 280), pos = wx.DefaultPosition,
+            size = (620, 280), pos = wx.DefaultPosition,
             style = wx.DEFAULT_DIALOG_STYLE 
                 | wx.MAXIMIZE_BOX 
                 | wx.MINIMIZE_BOX
