72b96f467e8d31da26167fb26607c1f972a58e5c
lightdruid <lightdruid>
2006-11-10 16:29:26 +0000

    no message

--- nanoicq/sandbox/ui/version.py
+++ nanoicq/sandbox/ui/version.py
@@ -1,13 +1,13 @@
 
 #
-# $Id: version.py,v 1.2 2006/03/14 15:47:56 lightdruid Exp $
+# $Id: version.py,v 1.3 2006/11/10 16:29:26 lightdruid Exp $
 #
 
 version_major = 0
 version_minor = 1
-version_rev = 2
+version_rev = 3
 version_status = 'alpha'
-version_name = 'Camelot'
+version_name = 'Karelia'
 
 version_string = '%d.%d.%d-%s' % (version_major, version_minor, version_rev, version_status)
 version_full_string = version_string + ' ("%s")' % version_name
